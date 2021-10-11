fb964655429f1c7690bb075ea4a693e43a40cd28
bungee <nikola.banek.kc2@gmail.com>
2021-10-11 18:06:17 +0100

    Added further code changes to Student and Module classes

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Module {
+    private String moduleName;
+    private int ID;
+    private Student[] students;
+    private CourseProgram[] associatedCourses;
+
+    public Module(String moduleName, int ID, Student[] students, CourseProgram[] associatedCourses){
+        this.moduleName = moduleName;
+        this.ID = ID;
+        this.students = students;
+        this.associatedCourses = associatedCourses;
+    }
+
+    public Module(){}
+
+    public void setModuleName(String moduleName){ this.moduleName = moduleName; }
+    public void setID(int ID){ this.ID = ID; }
+    public void setStudents(Student[] students){ this.students = students; }
+    public void setAssociatedCourses(CourseProgram[] associatedCourses){ this.associatedCourses = associatedCourses; }
+
+    public String getModuleName(){ return moduleName; }
+    public int getID(){ return ID; }
+    public Student[] getStudents(){ return students; }
+    public CourseProgram[] getAssociatedCourses(){ return associatedCourses; }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.Date;
 
 public class Student {
+
+    private String name, username;
+    private int age, ID;
+    private Module[] modules;
+    private CourseProgram[] courses;
+    private Date DOB;
+
+    public Student(String name, int age, int ID, Module[] modules, CourseProgram[] courses, Date DOB){
+        this.name = name;
+        this.age = age;
+        getUsername();
+        this.modules = modules;
+        this.courses = courses;
+        this.DOB = DOB;
+    }
+
+    public Student(){}
+
+    public void getUsername(){
+        String ageString = Integer.toString(age);
+        username = name + ageString;
+    }
+
+    public void setName(String name){ this.name = name; }
+    public void setAge(int age){ this.age = age; }
+    public void setID(int ID){ this.ID = ID; }
+    public void setModules(Module[] modules){ this.modules = modules; }
+    public void setCourses(CourseProgram[] courses){ this.courses = courses; }
+    public void setDOB(Date DOB){ this.DOB = DOB; }
+
+    public String getName(){ return name; }
+    public int getAge(){ return age; }
+    public int getID(){ return ID; }
+    public Module[] getModules(){ return modules; }
+    public CourseProgram[] getCourses(){ return courses; }
 }
